10737d958d027ebd3838c9ed781586abab1b9f40
Queke <nikolasgranadosi@gmail.com>
2021-11-21 18:47:22 -0300

    refactor: se optimiza formulario cambiando la re-renderización

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import React, { useState } from "react";
-import useFormulario from "./hooks/useFormulario";
-import Input from "./components/Input";
+
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Button from "./components/Button";
+import UserForm from "./components/UserForm";
 
 const App = () => {
-  const [formulario, handleChange, reset] = useFormulario({
-    name: "",
-    lastname: "",
-    correo: "",
-  });
   const [usuarios, setUsuarios] = useState([]);
 
-  const submit = (e) => {
-    e.preventDefault();
-    setUsuarios([...usuarios, formulario]);
-    reset();
+  const submit = (usuario) => {
+    setUsuarios([...usuarios, usuario]);
   };
-
+  console.log(usuarios);
   return (
     <div style={{ marginTop: "15%" }}>
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label="Nombre "
-                name="name"
-                value={formulario.name}
-                onChange={handleChange}
-                placeholder="Nombre"
-              />
-              <Input
-                label="apellido "
-                name="lastname"
-                value={formulario.lastname}
-                onChange={handleChange}
-                placeholder="Apellido"
-              />
-              <Input
-                label="Correo "
-                name="correo"
-                value={formulario.correo}
-                onChange={handleChange}
-                placeholder="Correo"
-              />
-              <Button>Enviar</Button>
-            </form>
+            <UserForm submit={submit} />
           </div>
         </Card>
         <Card>
